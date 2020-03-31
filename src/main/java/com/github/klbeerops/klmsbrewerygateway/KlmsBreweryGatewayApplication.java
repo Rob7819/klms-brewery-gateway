d513bf0456294e32349b71e3f4cf97a1130c8b6c
Rob <rjboyce@teksavvy.com>
2020-03-31 17:00:25 -0400

    Initial commit of project with minor adjustments (app properties and gitignore settings)

---
+++ src/main/java/com/github/klbeerops/klmsbrewerygateway/KlmsBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.klbeerops.klmsbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlmsBreweryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlmsBreweryGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/klbeerops/klmsbrewerygateway/KlmsBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.klbeerops.klmsbrewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlmsBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
